13a5f341cae61bece7e2366c95fe46b1ea24e20c
Anish Kelkar <136514319+KelkarAnish1@users.noreply.github.com>
2024-06-14 11:08:45 -0700

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+// Strings, Numbers, Boolean, null
+
+const name = 'john';
+const age = 30;
+
+// Concatenation
+console.log("my name is " + name + " and I am "+ age);
+// Template String
+const hello = `My name is ${name} and I am ${age}`;
+console.log(hello);
+
+const s = "hello world";
+console.log(s.toUpperCase());
+
+//Arrays - variables that hold multiple values
+const fruits = ['apples','oranges','pears',10];
+console.log(fruits);
